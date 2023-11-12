8de34317a0fc9b2778581ce0a9123c110cbbcd4d
Rayaja897 <145283482+Rayaja897@users.noreply.github.com>
2023-11-12 23:03:34 +0700

    [UPD] add footer content

--- src/pages/index.js
+++ src/pages/index.js
@@ -117,12 +117,40 @@ export default function Home() {
           <h2 className="text-[#fff]">
             Tingkatkan kinerja perusahaan <br /> dengan talent unggulan
           </h2>
-          <button className="btn-white btn-lg rounded">Mulai Dari Sekarang</button>
+          <button className="btn-white btn-lg rounded">
+            Mulai Dari Sekarang
+          </button>
         </div>
       </section>
       {/* end of CTA */}
 
       {/* footer */}
+      <footer className="bg-primary mt-[100px] py-[50px]">
+        <div className="container mx-auto">
+          <img
+            src="/images/logo-white.svg"
+            alt="logo"
+            width="178px"
+            height="50px"
+            className="mb-[10px]"
+          />
+          <p className="text-[#fff] mb-[50px]">
+            Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit. In
+            euismod ipsum et dui <br /> rhoncus auctor.
+          </p>
+
+          <hr />
+
+          <div className="flex justify-between mt-[20px]">
+            <p className="text-[#fff]">2020 Pewworld. All right reserved</p>
+
+            <div className="flex gap-[50px]">
+              <p className="text-[#fff]">Telepon</p>
+              <p className="text-[#fff]">Email</p>
+            </div>
+          </div>
+        </div>
+      </footer>
       {/* end of footer */}
     </main>
   );
