77406856423c78c9f5253e595ffb454ae335b146
Rayaja897 <145283482+Rayaja897@users.noreply.github.com>
2023-11-12 23:19:05 +0700

    [ADD] Logo and title

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Home | Peworld</title>
+        <link rel="icon" type="image/x-icon" href="/favicon.png"></link>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
